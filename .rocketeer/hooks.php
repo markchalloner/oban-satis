da77bfeb91d9c1e4b4ebc9b47bad2521e765d12d
Mark Challoner <mark.a.r.challoner@gmail.com>
2016-02-04 16:51:41 +0000

    Updated deploy config

--- .rocketeer/hooks.php
+++ .rocketeer/hooks.php
@@ -24,7 +24,7 @@ return [
     // Tasks to execute after the core Rocketeer Tasks
     'after'  => [
         'setup'   => [],
-        'deploy'  => [],
+        'deploy'  => ['bin/build'],
         'cleanup' => [],
     ],
 
